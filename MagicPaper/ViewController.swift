3bf95bcc085daaeefef44aed6c71dece11f0a345
Marko Jovanov <markojovanov999@gmail.com>
2021-08-31 11:39:04 +0200

    UpdateUI

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -10,57 +10,43 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
     @IBOutlet var sceneView: ARSCNView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARImageTrackingConfiguration()
         if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsPaperImages",
                                                                 bundle: Bundle.main) {
             configuration.trackingImages = trackedImages
             configuration.maximumNumberOfTrackedImages = 1
-            
         }
-        
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-        if let imageAnchord = anchor as? ARImageAnchor {
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            print("\(imageAnchor) -> Voa ti e Image Anchor")
             let videoNode = SKVideoNode(fileNamed: "harryPotterVideo.mp4")
             videoNode.play()
             let videoScene = SKScene(size: CGSize(width: 1080,
                                                   height: 720))
             videoNode.position = CGPoint(x: videoScene.size.width / 2,
                                          y: videoScene.size.height / 2)
-            videoNode.yScale = -1.0 
+            videoNode.yScale = -1.0
             videoScene.addChild(videoNode)
-            let plane = SCNPlane(width: imageAnchord.referenceImage.physicalSize.width,
-                                 height: imageAnchord.referenceImage.physicalSize.height)
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                                 height: imageAnchor.referenceImage.physicalSize.height)
             plane.firstMaterial?.diffuse.contents = videoScene
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -.pi / 2
